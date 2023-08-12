8fee4ad629d5e5c540c1133e33cf69c263702786
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-08-12 20:20:07 +0300

    longest substring

--- LeetCodeAlgorithms/Program.cs
+++ LeetCodeAlgorithms/Program.cs
@@ -5,11 +5,32 @@
         static void Main(string[] args)
         {
             string input = "abcabcbb";
+            int result = LengthOfLongestSubstring(input);
         }
 
-        public static int LengthOfLongestSubstring(string s)
+        public static int LengthOfLongestSubstring(string input)
         {
+            int n = input.Length;
+            int maxLength = 0;
+            int left = 0, right = 0;
+            HashSet<char> uniqueChars = new HashSet<char>();
 
+            while (right < n)
+            {
+                if (!uniqueChars.Contains(input[right]))
+                {
+                    uniqueChars.Add(input[right]);
+                    maxLength = Math.Max(maxLength, right - left + 1);
+                    right++;
+                }
+                else
+                {
+                    uniqueChars.Remove(input[left]);
+                    left++;
+                }
+            }
+
+            return maxLength;
         }
     }
 }
