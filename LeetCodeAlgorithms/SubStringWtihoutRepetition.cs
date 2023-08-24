36eea64e248df64795f295ab0c97c0834a6d334b
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-08-24 14:08:04 +0300

    new solution

---
+++ LeetCodeAlgorithms/SubStringWtihoutRepetition.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeAlgorithms
+{
+    internal class SubStringWtihoutRepetition
+    {
+        public static int LengthOfLongestSubstringSolution(string input)
+        {
+            int n = input.Length;
+            int maxLength = 0;
+            int left = 0, right = 0;
+            HashSet<char> uniqueChars = new HashSet<char>();
+
+            while (right < n)
+            {
+                if (!uniqueChars.Contains(input[right]))
+                {
+                    uniqueChars.Add(input[right]);
+                    maxLength = Math.Max(maxLength, right - left + 1);
+                    right++;
+                }
+                else
+                {
+                    uniqueChars.Remove(input[left]);
+                    left++;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeetCodeAlgorithms/TreeCousesSum.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeAlgorithms
+{
+    internal class TreeCousesSum
+    {
+        private  int SumClosestSolution(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int closestSum = nums[0] + nums[1] + nums[2];
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    int currentSum = nums[i] + nums[left] + nums[right];
+
+                    if (currentSum == target)
+                    {
+                        return currentSum;
+                    }
+
+                    if (Math.Abs(currentSum - target) < Math.Abs(closestSum - target))
+                    {
+                        closestSum = currentSum;
+                    }
+
+                    if (currentSum < target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return closestSum;
+        }
+    }
+}
